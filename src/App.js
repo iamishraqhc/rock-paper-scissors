f89b71cd612f3f7d6db04de9741b11bd972ccb92
Ishraq Haider Chowdhury <ishraq-haider.chowdhury@stud.uni-bamberg.de>
2021-09-08 00:40:31 +0200

    changed some names

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
   const [userChoice, setUserChoice] = useState('rock')
   const [computerChoice, setComputerChoice] = useState('rock')
-  const userPoints = useRef(0)
-  const computerPoints = useRef(0)
-  const [turnPoints, setTurnPoints] = useState(null)
+  const [userPoints, setUserPoints] = useState(0)
+  const [computerPoints, setComputerPoints] = useState(0)
+  const [turnResult, setTurnResult] = useState(null)
   const [result, setResult] = useState('Let\'s see who wins')
   const [gameOver, setGameOver] = useState(false)
   const choices = ['rock', 'paper', 'scissors']
@@ -21,52 +21,56 @@ const App = () => {
     setComputerChoice(randomChoice)
   }
 
-  const refresh = () => {
+  const reset = () => {
     window.location.reload()
   }
 
   useEffect(() => {
     const comboMoves = userChoice + computerChoice
-    if (userPoints.current <= 4 && computerPoints.current <= 4) {
+    if (userPoints <= 4 && computerPoints <= 4) {
       if (comboMoves === 'scissorspaper' || comboMoves === 'rockscissors' || comboMoves === 'paperrock') {
-        userPoints.current += 1
-        setTurnPoints('User gets the point!')
+        // userPoints.current += 1
+        const updatedUserPoints = userPoints + 1
+        setUserPoints(updatedUserPoints)
+        setTurnResult('User gets the point!')
+        if (updatedUserPoints === 5){
+          setResult('User Wins')
+          const gameOff = true
+          setGameOver(gameOff)
+        }
       }
 
       if (comboMoves === 'paperscissors' || comboMoves === 'scissorsrock' || comboMoves === 'rockpaper') {
-        computerPoints.current += 1
-        setTurnPoints('Computer gets the point!')
+        // computerPoints.current += 1
+        const updatedComputerPoints = computerPoints + 1
+        setComputerPoints(updatedComputerPoints)
+        setTurnResult('Computer gets the point!')
+        if (updatedComputerPoints === 5) {
+          setResult('Computer Wins')
+          const gameOff = true
+          setGameOver(gameOff)
+        }
       }
 
       if (comboMoves === 'paperpaper' || comboMoves === 'rockrock' || comboMoves === 'scissorsscissors') {
-        setTurnPoints('No one gets a point!')
+        setTurnResult('No one gets a point!')
       }
-    }  
-    if (userPoints.current === 5 && computerPoints.current < 5) {
-      setResult('User Wins')
-      setGameOver(true)
     }
-    if (computerPoints.current === 5 && userPoints.current < 5) {
-      setResult('Computer Wins')
-      setGameOver(true)
-    }
-  }, [computerChoice, userChoice, userPoints, computerPoints])
+  }, [computerChoice, userChoice])
 
   return (
     <div className="App">
       <h1 className='heading'>Rock-Paper-Scissors</h1>
       <div className='score'>
-        <h1>User Points: {userPoints.current}</h1>
-        <h1>Computer Points: {computerPoints.current}</h1>
+        <h1>User Points: {userPoints}</h1>
+        <h1>Computer Points: {computerPoints}</h1>
       </div>
 
       <div className='choice'>
         <div className='choice-user'>
-          <h1>Your choice:</h1>
           <img className='user-hand' src={`../images/${userChoice}.png`} alt=''></img>
         </div>
         <div className='choice-computer'>
-          <h1>Computer's choice:</h1>
           <img className='computer-hand' src={`../images/${computerChoice}.png`} alt=''></img>
         </div>
       </div>
@@ -80,13 +84,13 @@ const App = () => {
       </div>
       
       <div className='result'>
-        <h1>Turn Result: {turnPoints}</h1>
+        <h1>Turn Result: {turnResult}</h1>
         <h1>Final Result: {result}</h1>
       </div>
       
       <div className='button-div'>
         {gameOver && 
-          <button className='button' onClick={() => refresh()}>Restart Game?</button>
+          <button className='button' onClick={() => reset()}>Restart Game?</button>
         }
       </div>
     </div>
